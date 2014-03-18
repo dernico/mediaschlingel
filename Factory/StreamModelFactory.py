2cadc85b0c2b2358f6e5273246d654adadce87f7
dernico <nico.bolender.nb@gmail.com>
2014-03-18 08:52:36 +0100

    some changes

--- Factory/StreamModelFactory.py
+++ Factory/StreamModelFactory.py
@@ -5,9 +5,19 @@ class StreamModelFactory:
     def createFromUrl(self, url):
         model = StreamModel()
         model.Description = ""
-        model.Image = ""
+        model.Image = "keinbild.jpg"
         model.Format = url
         model.Name = url
         model.Stream = url
         model.Website = ""
+        return model
+
+    def createFromJson(self, json):
+        model = StreamModel()
+        model.Description = json["description"]
+        model.Image = json["image"]
+        model.Format = json["format"]
+        model.Name = json["name"]
+        model.Stream = json["stream"]
+        model.Website = json["website"]
         return model
