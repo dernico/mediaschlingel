ac61973050597408705d493ee36b25f09044e68e
dernico <nico.bolender.nb@gmail.com>
2014-12-09 19:40:38 +0100

    remove old public

--- public/models/youtubeModel.js
+++
@@ -1,9 +0,0 @@
-var youtubeModel = (function(){
-	return function(yt){
-		var self = this;
-		self.id = yt.id;
-		self.thumbnail = yt.thumbnail;
-		self.title = yt.title;
-		self.showPlayNext = ko.observable(yt.showPlayNext);
-	};
-})();
--- public/viewmodels/background.js
+++
@@ -1,6 +0,0 @@
-//var backgroundVM = [function(){
-pages.viewmodel("backgroundVM", ['background',function(backgroundService){
-	var self = this;
-	self.Cover = backgroundService.Cover;
-	backgroundService.setCover();
-}]);
